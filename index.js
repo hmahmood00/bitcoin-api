f45ca9c33f9d19d89a47b495e1881aff4ae67b5c
Haseeb Mahmood <haseebmahmood00@gmail.com>
2019-09-02 23:08:52 +0100

    added index.html and index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
+  // console.log(req.body.crypto);
+
+  var crypto = req.body.crypto;
+  var fiat = req.body.fiat;
+
+  var amount = req.body.amount;
+
+  // var baseURL = "https://apiv2.bitcoinaverage.com/indices/global";
+
+  // var finalURL = baseURL + crypto + fiat;
+
+  var options = {
+    url: "https://apiv2.bitcoinaverage.com/convert/global",
+    method: "GET",
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount: amount
+    }
+  };
+
+request(options, function(error, response, body){
+    // console.log(response.statusCode);
+    var data = JSON.parse(body);
+    var price = data.price;
+    console.log(price);
+    var currentDate = data.time;
+
+    res.write("<p>The Current Date is " + currentDate +"</p>");
+    res.write("<h1>" + amount +" " + crypto + " is currently worth " + price + " "+ fiat +"</h1>");
+
+    res.send();
+
+  });
+
+
+});
+
+app.listen(3000, function(){
+  console.log("server is running on port 3000");
+});
